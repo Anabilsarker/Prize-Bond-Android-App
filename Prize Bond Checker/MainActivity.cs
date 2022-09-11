1bfb2bce20c4e23dba5425c4ff819df617c4a3ea
Anabilsarker <sarkeranabil@gmail.com>
2022-09-11 18:20:23 +0600

    Add existing project files to Git

---
+++ Prize Bond Checker/MainActivity.cs
@@ -0,0 +1,148 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using AndroidX.CardView.Widget;
+using AndroidX.Core.View;
+using AndroidX.DrawerLayout.Widget;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Navigation;
+using Google.Android.Material.Snackbar;
+using System;
+
+namespace Prize_Bond_Checker
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
+    {
+        EditText bondNumber;
+        CardView addNewBondLayout;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            InitUIEvents();
+
+            //Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            //SetSupportActionBar(toolbar);
+            //DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+            //drawer.AddDrawerListener(toggle);
+            //toggle.SyncState();
+        }
+        public override void OnBackPressed()
+        {
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            if (drawer.IsDrawerOpen(GravityCompat.Start))
+            {
+                drawer.CloseDrawer(GravityCompat.Start);
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
+        }
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void InitUIEvents()
+        {
+            addNewBondLayout = FindViewById<CardView>(Resource.Id.add_bond_popup);
+            bondNumber = FindViewById<EditText>(Resource.Id.bond_number_entry_field);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            Button addnewbond = FindViewById<Button>(Resource.Id.add_bond);
+            addnewbond.Click += AddNewbond_Click;
+
+            Button cancel = FindViewById<Button>(Resource.Id.cancel);
+            cancel.Click += Cancel_Click;
+
+            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+        }
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            CardView addNewBondLayout = FindViewById<CardView>(Resource.Id.add_bond_popup);
+            addNewBondLayout.Visibility = ViewStates.Visible;
+
+            TextView textView = FindViewById<TextView>(Resource.Id.help_info);
+            textView.Visibility = ViewStates.Gone;
+        }
+        private async void AddNewbond_Click(object sender, EventArgs e)
+        {
+            QueryBonds queryBonds = new QueryBonds();
+            if (await queryBonds.PostRequest(bondNumber.Text))
+            {
+                View view = (View)sender;
+                Snackbar.Make(view, queryBonds.BondResult(), Snackbar.LengthLong).SetAction("Action", (View.IOnClickListener)null).Show();
+            }
+
+
+        }
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            bondNumber.Text = "";
+            addNewBondLayout.Visibility = ViewStates.Gone;
+        }
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+
+            //if (id == Resource.Id.nav_camera)
+            //{
+            //    // Handle the camera action
+            //}
+            //else if (id == Resource.Id.nav_gallery)
+            //{
+
+            //}
+            //else if (id == Resource.Id.nav_slideshow)
+            //{
+
+            //}
+            //else if (id == Resource.Id.nav_manage)
+            //{
+
+            //}
+            //else if (id == Resource.Id.nav_share)
+            //{
+
+            //}
+            //else if (id == Resource.Id.nav_send)
+            //{
+
+            //}
+
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            drawer.CloseDrawer(GravityCompat.Start);
+            return true;
+        }
+    }
+}
+
---
+++ Prize Bond Checker/QueryBonds.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Prize_Bond_Checker
+{
+    public class QueryBonds
+    {
+        private string ResponseValue { get; set; }
+        public async Task<bool> PostRequest(string value)
+        {
+            try
+            {
+                var values = new Dictionary<string, string>
+                {
+                    { "gsearch", $"{value}" }
+                };
+                var content = new FormUrlEncodedContent(values);
+
+                using (HttpClient client = new HttpClient())
+                {
+                    using (var response = await client.PostAsync("https://www.bb.org.bd/en/index.php/investfacility/prizebond", content))
+                    {
+                        string responseString = await response.Content.ReadAsStringAsync();
+                        ResponseValue = responseString;
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex.Message); return false; }
+        }
+        public string BondResult()
+        {
+            if (ResponseValue.Contains("Congratulations! "))
+            {
+                string str1 = ResponseValue[ResponseValue.IndexOf("Congratulations! ") + 17].ToString();
+                string str2 = ResponseValue[ResponseValue.IndexOf("Congratulations! ") + 18].ToString();
+                string str3 = ResponseValue[ResponseValue.IndexOf("Congratulations! ") + 19].ToString();
+
+                if (str2 == " ")
+                {
+                    return str1;
+                }
+                else if (str3 == " ")
+                {
+                    return str1 + str2;
+                }
+                else
+                {
+                    return str1 + str2 + str3;
+                }
+            }
+            else
+            {
+                return "0";
+            }
+        }
+    }
+}
